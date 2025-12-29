cf0d5489fc8d7120edb69c9dcd48216a92c13c0a
Oussama Bensarjane <obensarj@e1r13p1.1337.ma>
2025-12-29 17:57:27 +0100

    tets

---
+++ practice/testing.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// Defining a namespace called 'example'
+namespace example {
+    int variable = 42;
+
+    void display() {
+        std::cout << "This is the example namespace. " << std::endl;
+    }
+}
+
+// Another namespace with the same name as 'example', but in a different scope
+namespace another {
+    int variable = 99;
+
+		 void display() {
+        std::cout << "This is the another namespace. " << std::endl;
+			}
+}
+
+int main() {
+    // Accessing elements from the 'example' namespace
+    std::cout << "Value from 'example' namespace: " << example::variable << std::endl;
+    example::display();
+    // Accessing elements from the 'another' namespace
+    std::cout << "Value from 'another' namespace: " << another::variable << std::endl;
+		
+    return 0;
+}
