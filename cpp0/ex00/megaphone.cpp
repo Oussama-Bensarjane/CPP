ab1df284fc46b05eefbfe6a9ad0196c914aa0799
Oussama Bensarjane <obensarj@e1r13p2.1337.ma>
2025-12-31 19:51:39 +0100

    ok

---
+++ cpp0/ex00/megaphone.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+namespace Dchira 
+{
+	
+	void Display ()
+	{
+		std::cout << "My lovely Toown called "; 
+	}
+}
+
+int main()
+{
+	std::cout << Dchira::var << std::endl;
+}
