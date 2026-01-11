5ae4fda547a95da454f48f1154cb28a29b96edc3
Oussama-Bensarjane <tiznittop@gmail.com>
2026-01-11 02:04:39 +0100

    ok

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -1,1 +1,12 @@
 #include "Zombie.hpp"
+
+int main()
+{
+    int N = 5;
+    Zombie* horde = zombieHorde(N, "ahmed");
+    for (int i = 0; i < N; i++)
+        horde[i].announce();
+
+    delete[] horde;
+    return 0;
+}
