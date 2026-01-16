2a73af03d2b7bcd152eb10d6c10effcf89e3f0e5
Oussama-Bensarjane <tiznittop@gmail.com>
2026-01-16 01:10:39 +0100

    ok

---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,22 @@
+#include "Harl.hpp"
+
+int main()
+{
+	Harl harl;
+
+	std::cout << "Testing all complaint levels:" << std::endl;
+	std::cout << "==============================" << std::endl;
+	std::cout << std::endl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+
+	std::cout << "Testing invalid level:" << std::endl;
+	std::cout << "======================" << std::endl;
+	std::cout << std::endl;
+	harl.complain("NOOONEE");
+
+	return 0;
+}
