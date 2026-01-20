bc0931a2af85f09386ce50bbbb927d0aa5198380
Oussama-Bensarjane <tiznittop@gmail.com>
2026-01-20 10:39:10 +0100

    ok

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -1,17 +1,21 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+#include <iostream>
 
 class Fixed
 {
 private:
-	/* data */
+	int                 _fixedPointValue;
+	static const int    _fractionalBits = 8;
 public:
-	Fixed(/* args */);
+	Fixed();
+	Fixed(const Fixed& other);
+	Fixed& operator=(const Fixed& other);
 	~Fixed();
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
 };
-
-
-
 
 #endif
